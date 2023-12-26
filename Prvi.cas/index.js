3deaa30c9b6d3db5d26d3611a6da88b86b0ddf4e
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2023-12-26 20:51:14 +0100

    Pocetak

---
+++ Prvi.cas/index.js
@@ -0,0 +1,33 @@
+console.log("nas prvi cas");
+
+
+//prosti
+var ovojeNumber = 23;
+var ovojeBoolean = true;
+var ovojeString = "IT camp";
+var ovojeUnderfined = undefined;
+var ovojeNull = null;
+// Number,
+// Boolean,
+// String,
+// undefined,
+// null
+
+
+//slozeni
+
+var ovojeArray = [2,"D",true,false]
+// Array,
+// Object,
+// Function
+
+var osoba = {
+  ime: "Kemal",
+  prezime: "Skrijelj",
+  godine: "18"
+}
+
+function test (e) {
+console.log(e)
+}
+test(3)
