a3bab287d87ab2f23a3b78f5b99ed187de175ced
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-03-05 19:46:44 +0100

    update

--- Local-storage/index.js
+++ Local-storage/index.js
@@ -1,10 +1,23 @@
 // localStorage.setItem('test', prompt("unesi neku vrednost"))
-// console.log(localStorage.getItem('test'))
+// console.log(localStorage.getItem('test'))00
 
-if (!localStorage.getItem("jelKorisnikUlogovan")) {
-  localStorage.setItem("koriscnikoIme",prompt("Unesi korisnicko ime"))
-  localStorage.setItem("sifra", prompt("Unesi sifru"))
-  localStorage.setItem("jelKorisnikUlogovan",true)
-}else{
-  alert(`Dobro dosao ${localStorage.getItem("koriscnikoIme")}`)
-}
+// if (!localStorage.getItem("jelKorisnikUlogovan")) {
+//   localStorage.setItem("koriscnikoIme",prompt("Unesi korisnicko ime"))
+//   localStorage.setItem("sifra", prompt("Unesi sifru"))
+//   localStorage.setItem("jelKorisnikUlogovan",true)
+// }else{
+//   alert(`Dobro dosao ${localStorage.getItem("koriscnikoIme")}`)
+// }
+
+// const osoba = {
+//   ime:"Kemal",
+//   prezime: "Skrijelj",
+//   godine:18
+// }
+// Object.entries(osoba).forEach(([key, value]) => {
+//   console.log(key,value)
+// })
+
+localStorage.setItem('ulogovaniKorisnik','kemal')
+//                           key          value
+console.log(localStorage.getItem('ulogovaniKorisnik'))
